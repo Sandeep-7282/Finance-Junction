6de681ea9d875b67826db21e3ea1a55622b7e691
Sandeep <sandeepnadegouni7282@gmail.com>
2023-10-20 23:05:33 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,17 @@ const connectToMongo=require('./db');
 const express = require('express')
 require('dotenv').config();
 const app = express();
-app.use(express.json());
 var cors = require('cors')
+app.use(express.json());
 app.use(cors());
-
+app.use('/api/auth',require('./Routes/auth'));
+app.use('/api/notes',require('./Routes/notes'));
 connectToMongo();
 const port = 4100;
 app.get('/', (req, res) => {
   res.send('Hello World!');
   res.setHeader("Access-Control-Allow-Credentials","true");
 })
-app.use('/api/auth',require('./Routes/auth'));
-app.use('/api/notes',require('./Routes/notes'));
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
