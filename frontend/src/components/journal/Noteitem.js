a4f30940a0917d0653908d96c664fc5aaabc178b
Sandeep <sandeepnadegouni7282@gmail.com>
2023-11-11 09:55:08 +0530

    updated

--- frontend/src/components/journal/Noteitem.js
+++ frontend/src/components/journal/Noteitem.js
@@ -27,7 +27,7 @@ const Noteitem = (props) => {
   <div className={`card-body`} style={{backgroundColor:props.Mode==='light'?'#aab8b6':'#383838',boxShadow:props.Mode==='dark'?'3px 3px 3px rgba(255, 255, 255,0.8)':'3px 3px 3px rgba(0, 0, 0,0.7)',borderRadius:'5px'}}>
     <h4 className={` card-title`} style={{color:props.Mode==='dark'?'#088395':'#025933'}}>{note.title}</h4>
     <p className={` card-text text-${props.Mode==='light'?'dark':'light'}`}>{note.description}</p>
-    <small className={`text-${props.Mode==='light'?'success':'info'}`}>{note.tag}</small>
+    <small className={`tag-text text-${props.Mode==='light'?'success':'info'}`}>{note.tag}</small>
     <br />
     {!localStorage.getItem('token')||jwt_decode(localStorage.getItem('token')).user.id===id_data?
    <></>: <div className="d-flex justify-content-between align-items-center mx-2">
@@ -36,7 +36,7 @@ const Noteitem = (props) => {
     <i className={`fa-solid fa-trash mx-2 my-2 text-${props.Mode==='light'?'success':'info'}`} onClick={()=>{deletenote(note._id)}} id="crud-bt"></i>
     </div>
     <div>
-     <p className={` card-text date text-${props.Mode==='light'?'dark':'info'}`}> {`${new Date(note.date).getHours()}:${new Date(note.date).getMinutes()}:${new Date(note.date).getSeconds()} hrs, ${new Date(note.date).getDate()}/${new Date(note.date).getMonth()+1}/${new Date(note.date).getFullYear()}`}</p>
+     <p className={` card-text date text-${props.Mode==='light'?'dark':'info'}`}> {`${new Date(note.date).getHours()}:${new Date(note.date).getMinutes()}:${new Date(note.date).getSeconds()} hrs; ${new Date(note.date).getDate()}/${new Date(note.date).getMonth()+1}/${new Date(note.date).getFullYear()}`}</p>
     </div>
     </div>
     }
